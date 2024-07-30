107bd60ddd773ed368a39a0f907e8b10f0ba1457
VanbrosChem <Vanbrostrade@gmail.com>
2024-07-30 18:55:07 +0200

    Update MarketingSection.tsx

--- src/components/MarketingSection.tsx
+++ src/components/MarketingSection.tsx
@@ -9,19 +9,19 @@ function MarketingSection() {
             <img src="product1.jpg" alt="PMMA Light Guide Plate" className="bd-placeholder-img rounded-circle" width="150" height="150"/>
             <h2>PMMA Light Guide Plate</h2>
             <p>Products produced by Polymethyl methacrylate (PMMA) Particles.</p>
-            <p><a className="btn btn-secondary" href="#">Contact for details &raquo;</a></p>
+            <p><a className="btn btn-secondary">Contact for details &raquo;</a></p>
           </div>
           <div className="col-lg-4">
             <img src="product2.jpg" alt="Rubber Machinery" className="bd-placeholder-img rounded-circle" width="150" height="150"/>
             <h2>Rubber and Plastic Machinery</h2>
             <p>Manufacture Industrial moulds for rubber and plastic working machinery.</p>
-            <p><a className="btn btn-secondary" href="#">Contact for details &raquo;</a></p>
+            <p><a className="btn btn-secondary">Contact for details &raquo;</a></p>
           </div>
           <div className="col-lg-4">
             <img src="product3.jpg" alt="Acrylic Sheet" className="bd-placeholder-img rounded-circle" width="150" height="150"/>
             <h2>Acrylic Sheets</h2>
             <p>Products produced by Polymethyl methacrylate (PMMA) Particles.</p>
-            <p><a className="btn btn-secondary" href="#">Contact for details &raquo;</a></p>
+            <p><a className="btn btn-secondary">Contact for details &raquo;</a></p>
           </div>
         </div>
       </div>
