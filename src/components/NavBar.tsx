f2f61184f4cc6a344f2cf0c845e5dc05021eca28
VanbrosChem <Vanbrostrade@gmail.com>
2024-07-30 18:55:31 +0200

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,14 +5,14 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Van Bros</a>
+                <a className="navbar-brand" href="#top">Van Bros</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarCollapse">
                     <ul className="navbar-nav me-auto mb-2 mb-md-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            <a className="nav-link active" aria-current="page" href="#top">Home</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#Feature1">Chemistry</a>
